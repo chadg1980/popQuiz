77f9a9c87154216281dec69cc8e188ce0fc4360e
Chad H <chadg1980@gmail.com>
2017-01-12 14:44:48 -0800

    Done with the layout and style, Green for the Emerald City.

--- app/src/main/java/com/h/chad/popquiz/MainActivity.java
+++ app/src/main/java/com/h/chad/popquiz/MainActivity.java
@@ -1,7 +1,14 @@
 package com.h.chad.popquiz;
 
+import android.graphics.Color;
+import android.os.CountDownTimer;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
